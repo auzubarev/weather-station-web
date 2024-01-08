f7a2028135f508ede2ddc16226a4e7f51711aa27
Alex Zubarev <auzubarev@gmail.com>
2024-01-08 19:17:19 +0700

    Auto deploy: fix yml and script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -6,24 +6,24 @@ echo "Deployment started ..."
 
 # Enter maintenance mode or return true
 # if already is in maintenance mode
-(php artisan down) || true
+(php8.1 artisan down) || true
 
 # Pull the latest version of the app
 git pull
 
 # Install composer dependencies
-composer-php8.1 install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+php8.1 ~/.local/bin/composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
 # Clear the old cache
-php artisan clear-compiled
+php8.1 artisan clear-compiled
 
 # Recreate cache
-php artisan optimize
+php8.1 artisan optimize
 
 # Run database migrations
-php artisan migrate --force
+php8.1 artisan migrate --force
 
 # Exit maintenance mode
-php artisan up
+php8.1 artisan up
 
 echo "Deployment finished!"
