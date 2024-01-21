87ce811037238ce864293f9a763b4668b015dcf0
Alex Zubarev <auzubarev@gmail.com>
2024-01-21 22:36:41 +0700

    Add wireui

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    <x-button primary label="Primary" />
+                    <x-input
+                        label="Name"
+                        placeholder="your name"
+                    />
                     {{ __("You're logged in!") }}
                 </div>
             </div>
