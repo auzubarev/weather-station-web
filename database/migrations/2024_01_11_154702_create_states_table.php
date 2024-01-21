18c22512c16167195fca364ec1f28da00dffc702
Alex Zubarev <auzubarev@gmail.com>
2024-01-21 22:35:54 +0700

    Add state model

---
+++ database/migrations/2024_01_11_154702_create_states_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->float('temperature')->default(0);
+            $table->float('pressure')->default(0);
+            $table->float('humidity')->default(0);
+            $table->string('period');
+            $table->dateTime('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('states');
+    }
+};
