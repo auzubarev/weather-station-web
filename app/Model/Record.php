b38a5fabf3b63d25e1cc13fd7cacf1a9328d444b
Alex Zubarev <auzubarev@gmail.com>
2024-02-04 22:52:52 +0700

    Add Eloquent

---
+++ app/Model/Record.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Model;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Record extends Model {
+    protected $table = 'records';
+    protected $primaryKey = 'id';
+
+}
