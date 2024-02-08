dd135f4c98c02fcd0aff4f9278662403b361889b
Alex Zubarev <auzubarev@gmail.com>
2024-02-08 11:55:05 +0700

    Trailing slash

--- route.php
+++ route.php
@@ -1,5 +1,6 @@
 <?php
 use App\Service\View;
+use Middlewares\TrailingSlash;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -10,5 +11,7 @@ $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write(View::r('index'));
     return $response;
 });
+
+$app->add(new TrailingSlash(true));
 
 $app->run();
