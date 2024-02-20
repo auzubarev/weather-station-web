463c4d8d26ca81d059f26e67554ded544f648642
Alex Zubarev <auzubarev@gmail.com>
2024-02-20 23:00:53 +0700

    add timezone

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/config.php';
-
+date_default_timezone_set(TIME_ZONE);
 $capsule = new \Illuminate\Database\Capsule\Manager;
 $capsule->addConnection([
     'driver' => 'mysql',
--- config.php
+++ config.php
@@ -1,1 +1,10 @@
 <?php
+
+define('MYSQL_HOST', '');
+define('MYSQL_DB', '');
+define('MYSQL_LOGIN', '');
+define('MYSQL_PASSWORD', '');
+define('MYSQL_PORT', '');
+define('ADMIN_TOKEN', '');
+define('URL', '');
+define('TIME_ZONE', '');
